9ae1bc82acc00a5c659074c0e67260f14e7be1b6
brederbreder <mtbreder@gmail.com>
2019-10-26 10:00:58 -0300

    finalizado o letterbox + readme

---
+++ Letterbox/README.txt
@@ -0,0 +1,14 @@
+Trabalho 2 TP2 2019/2
+Exercises in programming style
+Style 12 - Letterbox
+
+How to compile the program:
+g++ -o exec -std=c++11 -g -pedantic -Wall letterbox.cpp 
+
+How to execute the program:
+./exec ./pride_and_prejudice.txt
+
+Letterbox é um estilo de programacao em que dividimos o programa em modulos(classes)
+e para fazer a comunicação entre as classes(troca de mensagens), envia-se um vetor de strings
+como ser fosse uma carta para a outra classe, que recebe como uma função Dispatch(), lê a string
+e encaminha para a função correta, por isso o nome Letterbox(caixa de correio)
--- Letterbox/letterbox.cpp
+++ Letterbox/letterbox.cpp
@@ -3,6 +3,9 @@
 #include <fstream>
 #include <vector>
 #include <regex>
+#include <list>
+#include <map>
+
 
 // DataStorageManager x = DataStorageManager();
 // DataStorageManager *x = new DataStorageManager();
@@ -10,33 +13,139 @@
 
 class DataStorageManager{
     public:
-        void dispatch(const std::vector<std::string> &message){
+        std::list<std::string> dispatch(const std::vector<std::string> &message){
             if(!message[0].compare("init")){
                 this->init(message[1]);
+            }else if(!message[0].compare("words")){
+                this->words();
+            }else{
+                throw std::invalid_argument("Message not understoood\n");
             }
+            return this->list_of_words;
         }
 
         void init(const std::string &path){
-            std::regex e("[\\w_]+");
+            // std::regex e("[\\w_]+");
             std::fstream in(path);
             std::string x;
             while (in >> x){
-                this->data += x;    
+                this->data += x;
+                this->data += ' ';    
             }
-            for (std::sregex_iterator it(data.begin(), data.end(), e), it_end; it != it_end; ++it) {
-                std::cout << (*it)[0] << "\n";
+            for(unsigned int i=0; i<this->data.length(); i++){
+                if((this->data[i] < 'a' || this->data[i] > 'z') && (this->data[i] < 'A' || this->data[i] > 'Z')){
+                    this->data[i] = ' ';
+                }
             }
+
+            std::transform(this->data.begin(), this->data.end(), this->data.begin(),
+                [](unsigned char c){
+                return std::tolower(c); 
+            });
+
+
+            for(unsigned int i=0; i<this->data.size(); i++){
+                if(this->data[i] >= 'a' && this->data[i] <= 'z'){
+                    dataOut += data[i];
+                }
+                if(this->data[i] == ' ' && this->data[i+1] != ' ' && this->data[i+1] != '\0'){
+                    dataOut += data[i];
+                }
+            }
+            // for (std::sregex_iterator it(data.begin(), data.end(), e), it_end; it != it_end; ++it) {
+            //     std::cout << (*it)[0] << "\n";
+            // }
         }
+
+        std::list<std::string> words(){
+            std::string aux;
+            for(unsigned int i=0; i<=this->dataOut.size(); i++){
+                if(this->dataOut[i] != ' '){
+                    aux += this->dataOut[i];
+                }
+                if(this->dataOut[i] == ' ' || this->dataOut[i] == '\0'){
+                    list_of_words.push_back(aux);
+                    aux = "";
+                }
+            }
+            return list_of_words;
+        }
+
+
     private:
         std::string data;
+        std::string dataOut;
+        std::list<std::string> list_of_words;
 };
 
 class StopWordManager{
+    public:
+        bool dispatch(const std::vector<std::string> &message){
+            bool x;
+            if(!message[0].compare("init")){
+                this->init();
+            }else if(!message[0].compare("is_stop_word")){
+                auto m = message[1];
+                x = this->is_stop_word(m);
+            }else{
+                throw std::invalid_argument("Message not understoood\n");
+            }
+            return x;
+        }
 
+        void init(){
+            std::fstream in;
+            in.open("stop_words.txt");
+            std::string x, aux;
+            in >> x;
+            
+            for(unsigned int i=0; i<=x.size(); i++){
+                if(x[i] == ',' || x[i] == '\0'){
+                    this->stop_words.push_back(aux);
+                    aux = "";
+                }else{
+                    aux += x[i];
+                }
+            }
+        }
+
+        bool is_stop_word(std::string &word){
+            std::string x = word;
+            bool ret;
+            for(auto p:this->stop_words){
+                if(!p.compare(x)){
+                    ret = true;
+                    break;
+                }else{
+                    ret = false;;
+                }
+            }
+            return ret;
+        }
+
+    private:
+        std::list<std::string> stop_words;
 };
 
 class WordFrequencyManager{
+    public:
 
+        std::map<std::string, int> word_freq;
+
+        void dispatch(const std::vector<std::string> &message){
+            if(!message[0].compare("increment_count")){
+                auto s = message[1];
+                this->increment_cout(s);
+            }else{
+                throw std::invalid_argument("Message not understoood\n");
+            }
+        }
+
+        void increment_cout(std::string &word){
+            word_freq[word] += 1;
+        }
+
+    private:
 };
 
 class WordFrequencyController{
@@ -45,10 +154,10 @@ class WordFrequencyController{
             if(!message[0].compare("init")){
                 auto m = message[1];
                 this->init(m);
-            }else if(message[0].compare("run")){
-
+            }else if(!message[0].compare("run")){
+                this->run();
             }else{
-                throw std::invalid_argument("mensagem nao compreendida");
+                throw std::invalid_argument("Message not understoood\n");
             }
         }
 
@@ -57,6 +166,25 @@ class WordFrequencyController{
             this->swm = StopWordManager();
             this->wfm = WordFrequencyManager();
             this->dsm.dispatch({"init", path});
+            this->swm.dispatch({"init"});
+        }
+
+        void run(){
+            std::list<std::string> aux;
+            std::string s;
+            bool x;
+            aux = this->dsm.dispatch({"words"});
+            
+            for(auto p:aux){
+                x = this->swm.dispatch({"is_stop_word", p});
+                if(!x){
+                    this->wfm.dispatch({"increment_count", p});
+                }
+            }
+
+            for(auto p:this->wfm.word_freq){
+                std::cout << p.first << " - " << p.second << "\n";
+            }
         }
         
     private:
@@ -69,5 +197,5 @@ class WordFrequencyController{
 int main(const int argc, const char **argv){
     auto wfcontroller = WordFrequencyController();
     wfcontroller.dispatch({"init", *++argv});
-    
+    wfcontroller.dispatch({"run"});
 }
