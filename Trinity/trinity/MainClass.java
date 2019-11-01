e1d48e658e959bdf348238c6f438638938d121a0
Fernando F. Cordeiro <37345898+FernandoCordeiro@users.noreply.github.com>
2019-11-01 15:59:27 -0300

    Update MainClass.java

--- Trinity/trinity/MainClass.java
+++ Trinity/trinity/MainClass.java
@@ -1,24 +1,5 @@
 package trinity;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List; 
-import java.util.Scanner;
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Iterator;
-import java.util.Set;
-
 
 public class MainClass {
 
