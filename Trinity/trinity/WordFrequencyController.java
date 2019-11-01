9a49932a921105edd4f3438e1fb1e13c354301a1
Fernando F. Cordeiro <37345898+FernandoCordeiro@users.noreply.github.com>
2019-11-01 16:00:27 -0300

    Update WordFrequencyController.java

--- Trinity/trinity/WordFrequencyController.java
+++ Trinity/trinity/WordFrequencyController.java
@@ -1,23 +1,6 @@
 package trinity;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List; 
 import java.util.Scanner;
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Iterator;
-import java.util.Set;
 
 public class WordFrequencyController {
 	private WordFrequenciesModel model;
@@ -36,9 +19,10 @@ public class WordFrequencyController {
 		while (sc.hasNext()) {
 			file = sc.next();
 			if (file.equalsIgnoreCase("end")) break;
+			this.model.freqs.clear();
 			this.model.update(file);
 			this.view.render();
-			System.out.println("Next file: ");
+			System.out.println("Next file: (Type 'END' to finish)");
 		}
 		sc.close();
 	}
